f67279de815dc3d28eb3b99753ad36e8ea843062
Ilya Scharrenbroich <ilya.j.s@gmail.com>
2019-11-11 16:46:38 -0500

    Upgrade to use Rails ~> 5.1.0

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 solidus_version = ENV["SOLIDUS_VERSION"] || "~> 1.3.0"
-rails_version = ENV["RAILS_VERSION"] || "~> 4.2.0"
+rails_version = ENV["RAILS_VERSION"] || "~> 5.1.0"
 
 gem "solidus", solidus_version
 gem "rails", rails_version
--- lib/klarna_gateway/controllers/notifications_controller.rb
+++ lib/klarna_gateway/controllers/notifications_controller.rb
@@ -4,20 +4,20 @@ module KlarnaGateway
       payment_source = Spree::KlarnaCreditPayment.find_by_order_id!(params[:order_id])
 
       case params[:event_type]
-        when "FRAUD_RISK_ACCEPTED"
+        when 'FRAUD_RISK_ACCEPTED'
           payment_source.accept!
-        when "FRAUD_RISK_REJECTED", "FRAUD_RISK_STOPPED"
+        when 'FRAUD_RISK_REJECTED', 'FRAUD_RISK_STOPPED'
           payment_source.reject!
       end
 
       payment_source.payments.first.notify!(params)
 
       payment_source.order.touch
-      render text: "ok"
+      render plain: 'ok'
     end
 
     def push
-      render text: "ok"
+      render plain: 'ok'
     end
   end
 end
